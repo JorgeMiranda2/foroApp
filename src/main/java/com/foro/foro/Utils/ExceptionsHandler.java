339ac7266eb16ca19f1310802c63f9c0407ac1ad
JorgeMiranda <jemiranda003@gmail.com>
2025-01-19 01:16:52 -0500

    * Created a data Initialaizer class for ensure user profile exists * Local variables added * Corrected Http status validations

--- src/main/java/com/foro/foro/Utils/ExceptionsHandler.java
+++ src/main/java/com/foro/foro/Utils/ExceptionsHandler.java
@@ -2,6 +2,7 @@ package com.foro.foro.Utils;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -25,7 +26,14 @@ public class ExceptionsHandler {
     public ResponseEntity<String> handleIllegalArgumentException (Exception ex) {
         // Aquí se maneja la excepción y se devuelve una respuesta adecuada
         System.out.println("Resource not found: " + ex.getMessage());
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(AuthenticationCredentialsNotFoundException.class)
+    public ResponseEntity<String> AuthenticationCredentialsNotFoundException (Exception ex) {
+        // Aquí se maneja la excepción y se devuelve una respuesta adecuada
+        System.out.println("Incorrect credentials: " + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
     }
 
 }
